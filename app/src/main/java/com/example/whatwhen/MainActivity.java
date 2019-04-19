4efeb0bee374193091d16ebb7a225e3c7d464a38
Zack Saunders <zss3@illinois.edu>
2019-04-19 16:11:37 -0500

    add assignment and remove assignment methods and onclicks added

--- app/src/main/java/com/example/whatwhen/MainActivity.java
+++ app/src/main/java/com/example/whatwhen/MainActivity.java
@@ -4,11 +4,19 @@ import android.os.Bundle;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.support.annotation.NonNull;
+import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class MainActivity extends AppCompatActivity {
+    private int currentIndex;
     private TextView mTextMessage;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
@@ -38,6 +46,52 @@ public class MainActivity extends AppCompatActivity {
         BottomNavigationView navView = findViewById(R.id.nav_view);
         mTextMessage = findViewById(R.id.InputYourAssignments);
         navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        final Button addButton = findViewById(R.id.addButton);
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addAssignment();
+            }
+        });
+        final Button removeButton = findViewById(R.id.removeButton);
+        removeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                removeAssignment();
+            }
+        });
     }
+    public void addAssignment() {
+        EditText course = findViewById(R.id.courseInput);
+        EditText assignment = findViewById(R.id.assignmentInput);
+        EditText hrs = findViewById(R.id.hrsInput);
+        EditText mins = findViewById(R.id.minsInput);
+        String setCourse = course.getText().toString();
+        String setName = assignment.getText().toString();
+        int setHrs = Integer.parseInt(hrs.getText().toString());
+        int setMins = Integer.parseInt(mins.getText().toString());
+        Assignment toAdd = new Assignment();
+        toAdd.selected = false;
+        toAdd.course = setCourse;
+        toAdd.name = setName;
+        toAdd.hrs = setHrs;
+        toAdd.mins = setMins;
+        assignmentList.add(toAdd);
+    }
+    public void removeAssignment() {
+        assignmentList.remove(currentIndex);
+    }
+
+    private class Assignment {
+        boolean selected = false;
+        String course;
+        String name;
+        int hrs;
+        int mins;
+    }
+
+    private List<Assignment> assignmentList = new ArrayList();
+
+
 
 }
